896f5a55560d13143a781fef02423cbc4f2f28ec
Deng Xinlin <dengxinlin2429@gmail.com>
2023-08-07 22:07:52 +0800

    reformat code into Google style
    Signed-off-by: Deng Xinlin <dengxinlin2429@gmail.com>

--- db/status.cc
+++ db/status.cc
@@ -1,4 +1,5 @@
 #include "leveldb/status.h"
+
 #include <cassert>
 #include <cstdint>
 #include <cstdio>
@@ -6,69 +7,67 @@
 #include <string>
 
 namespace leveldb {
-const char* Status::CopyStatus(const char* state)
-{
-    uint32_t size;
-    std::memcpy(&size, state, sizeof(size));
-    char* result = new char[size + 5];
-    std::memcpy(result, state, size + 5);
+const char* Status::CopyStatus(const char* state) {
+  uint32_t size;
+  std::memcpy(&size, state, sizeof(size));
+  char* result = new char[size + 5];
+  std::memcpy(result, state, size + 5);
+  return result;
+}
+
+Status::Status(Code code, const Slice& msg, const Slice& msg2) {
+  assert(code != kOk);
+  const uint32_t len1 = msg.size();
+  const uint32_t len2 = msg2.size();
+  const uint32_t size = (len2 ? (len2 + 2) : 0) + len1;
+  char* result = new char[size + 5];
+  std::memcpy(result, &size, sizeof(size));
+  result[4] = static_cast<char>(code);
+  std::memcpy(result + 5, msg.data(), len1);
+  if (len2) {
+    result[5 + len1] = ':';
+    result[6 + len1] = ' ';
+    std::memcpy(result + 7 + len1, msg2.data(), len2);
+  }
+  status_ = result;
+}
+
+std::string Status::ToString() const {
+  if (status_ == nullptr) {
+    return "OK";
+  } else {
+    char tmp[20];
+    const char* type;
+    switch (code()) {
+      case kOk:
+        type = "OK";
+        break;
+      case kNotFound:
+        type = "NotFound";
+        break;
+      case kCorruption:
+        type = "Corruption";
+        break;
+      case kNotSupported:
+        type = "NotSupported";
+        break;
+      case kInvalidArgument:
+        type = "InvalidArgument";
+        break;
+      case kIOError:
+        type = "IOError";
+        break;
+      default:
+        std::snprintf(tmp, sizeof(tmp), "Unknown (%d)",
+                      static_cast<int>(code()));
+        type = tmp;
+        break;
+    }
+    std::string result(type);
+    uint32_t length;
+    std::memcpy(&length, status_, sizeof(length));
+    result.append(status_ + 5, length);
     return result;
+  }
 }
-
-Status::Status(Code code, const Slice& msg, const Slice& msg2)
-{
-    assert(code != kOk);
-    const uint32_t len1 = msg.size();
-    const uint32_t len2 = msg2.size();
-    const uint32_t size = (len2 ? (len2 + 2) : 0) + len1;
-    char* result = new char[size + 5];
-    std::memcpy(result, &size, sizeof(size));
-    result[4] = static_cast<char>(code);
-    std::memcpy(result + 5, msg.data(), len1);
-    if (len2) {
-        result[5 + len1] = ':';
-        result[6 + len1] = ' ';
-        std::memcpy(result + 7 + len1, msg2.data(), len2);
-    }
-    status_ = result;
-}
-
-std::string Status::ToString() const
-{
-    if (status_ == nullptr) {
-        return "OK";
-    } else {
-        char tmp[20];
-        const char* type;
-        switch (code()) {
-        case kOk:
-            type = "OK";
-            break;
-        case kNotFound:
-            type = "NotFound";
-            break;
-        case kCorruption:
-            type = "Corruption";
-            break;
-        case kNotSupported:
-            type = "NotSupported";
-            break;
-        case kInvalidArgument:
-            type = "InvalidArgument";
-            break;
-        case kIOError:
-            type = "IOError";
-            break;
-        default:
-            std::snprintf(tmp, sizeof(tmp), "Unknown (%d)", static_cast<int>(code()));
-            type = tmp;
-            break;
-        }
-        std::string result(type);
-        uint32_t length;
-        std::memcpy(&length, status_, sizeof(length));
-        result.append(status_+5, length);
-        return result;
-    }
-}
-}
+}  // namespace leveldb
--- include/leveldb/status.h
+++ include/leveldb/status.h
@@ -1,117 +1,104 @@
 #ifndef LEVELDB_STATUS_H_
 #define LEVELDB_STATUS_H_
 
+#include <string>
+
 #include "leveldb/export.h"
 #include "leveldb/slice.h"
-#include <string>
 
 namespace leveldb {
 class LEVELDB_EXPORT Status {
-public:
-    /*success*/
-    Status() noexcept
-        : status_(nullptr)
-    {
-    }
-    ~Status() { delete[] status_; }
-    Status(const Status& rhs);
-    Status& operator=(const Status& rhs);
-    Status(Status&& rhs) noexcept
-        : status_(rhs.status_)
-    {
-        rhs.status_ = nullptr;
-    }
-    Status& operator=(Status&& rhs) noexcept;
+ public:
+  /*success*/
+  Status() noexcept : status_(nullptr) {}
+  ~Status() { delete[] status_; }
+  Status(const Status& rhs);
+  Status& operator=(const Status& rhs);
+  Status(Status&& rhs) noexcept : status_(rhs.status_) {
+    rhs.status_ = nullptr;
+  }
+  Status& operator=(Status&& rhs) noexcept;
 
-    static Status OK() { return Status(); }
+  static Status OK() { return Status(); }
 
-    static Status NotFound(const Slice& msg, const Slice& msg2 = Slice())
-    {
-        return Status(kNotFound, msg, msg2);
-    }
+  static Status NotFound(const Slice& msg, const Slice& msg2 = Slice()) {
+    return Status(kNotFound, msg, msg2);
+  }
 
-    static Status Corruption(const Slice& msg, const Slice& msg2 = Slice())
-    {
-        return Status(kCorruption, msg, msg2);
-    }
+  static Status Corruption(const Slice& msg, const Slice& msg2 = Slice()) {
+    return Status(kCorruption, msg, msg2);
+  }
 
-    static Status NotSupported(const Slice& msg, const Slice& msg2 = Slice())
-    {
-        return Status(kNotSupported, msg, msg2);
-    }
+  static Status NotSupported(const Slice& msg, const Slice& msg2 = Slice()) {
+    return Status(kNotSupported, msg, msg2);
+  }
 
-    static Status InvalidArgument(const Slice& msg, const Slice& msg2 = Slice())
-    {
-        return Status(kInvalidArgument, msg, msg2);
-    }
+  static Status InvalidArgument(const Slice& msg, const Slice& msg2 = Slice()) {
+    return Status(kInvalidArgument, msg, msg2);
+  }
 
-    static Status IOError(const Slice& msg, const Slice& msg2)
-    {
-        return Status(kIOError, msg, msg2);
-    }
+  static Status IOError(const Slice& msg, const Slice& msg2) {
+    return Status(kIOError, msg, msg2);
+  }
 
-    bool ok() const { return status_ == nullptr; }
+  bool ok() const { return status_ == nullptr; }
 
-    bool IsNotFound() const { return code() == kNotFound; }
+  bool IsNotFound() const { return code() == kNotFound; }
 
-    bool IsCorruption() const { return code() == kCorruption; }
+  bool IsCorruption() const { return code() == kCorruption; }
 
-    bool IsNotSupportedError() const { return code() == kNotSupported; }
+  bool IsNotSupportedError() const { return code() == kNotSupported; }
 
-    bool IsInvalidArgument() const { return code() == kInvalidArgument; }
+  bool IsInvalidArgument() const { return code() == kInvalidArgument; }
 
-    bool IsIOError() const { return code() == kIOError; }
+  bool IsIOError() const { return code() == kIOError; }
 
-    std::string ToString() const;
+  std::string ToString() const;
 
-private:
-    enum Code {
-        kOk = 0,
-        kNotFound = 1,
-        kCorruption = 2,
-        kNotSupported = 3,
-        kInvalidArgument = 4,
-        kIOError = 5
-    };
+ private:
+  enum Code {
+    kOk = 0,
+    kNotFound = 1,
+    kCorruption = 2,
+    kNotSupported = 3,
+    kInvalidArgument = 4,
+    kIOError = 5
+  };
 
-    Code code() const
-    {
-        return (status_ == nullptr) ? kOk : static_cast<Code>(status_[4]);
-    }
-    /*
-        msg:error msg
-        msg2:description
-    */
-    Status(Code code, const Slice& msg, const Slice& msg2);
+  Code code() const {
+    return (status_ == nullptr) ? kOk : static_cast<Code>(status_[4]);
+  }
+  /*
+      msg:error msg
+      msg2:description
+  */
+  Status(Code code, const Slice& msg, const Slice& msg2);
 
-    static const char* CopyStatus(const char* s);
-    /*
-        status_[0..3] = message length
-        status_[4] = code
-        status_[5..] = message
-    */
-    const char* status_;
+  static const char* CopyStatus(const char* s);
+  /*
+      status_[0..3] = message length
+      status_[4] = code
+      status_[5..] = message
+  */
+  const char* status_;
 };
 
-inline Status::Status(const Status& rhs)
-{
+inline Status::Status(const Status& rhs) {
+  status_ = (rhs.status_ == nullptr) ? nullptr : CopyStatus(rhs.status_);
+}
+
+inline Status& Status::operator=(const Status& rhs) {
+  if (this != &rhs) {
+    delete[] status_;
     status_ = (rhs.status_ == nullptr) ? nullptr : CopyStatus(rhs.status_);
+  }
+  return *this;
 }
 
-inline Status& Status::operator=(const Status& rhs)
-{
-    if (this != &rhs) {
-        delete[] status_;
-        status_ = (rhs.status_ == nullptr) ? nullptr : CopyStatus(rhs.status_);
-    }
-    return *this;
+inline Status& Status::operator=(Status&& rhs) noexcept {
+  std::swap(status_, rhs.status_);
+  return *this;
 }
-
-inline Status& Status::operator=(Status&& rhs) noexcept
-{
-    std::swap(status_, rhs.status_);
-    return *this;
-}
-}
+}  // namespace leveldb
 
 #endif
